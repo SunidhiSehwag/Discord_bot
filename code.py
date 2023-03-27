a6745561363679eac477f6e1e1286c5e0885cc29
Sunidhi <120239019+SunidhiSehwag@users.noreply.github.com>
2023-03-27 12:00:02 +0530

    Discord bot code link

--- code.py
+++ code.py
@@ -1,8 +1,7 @@
+print("hi")
 user_input = input("enter a command\n")
 
 if user_input.lower() == "ping":
     print("pong")
-    user_input1=input("enter command\n")
-    if user_input1.lower() == "hi":
-       print("greetings")
+    
     
