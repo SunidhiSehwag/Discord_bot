2726cb8d0c4faec9f437e4ee231c3c8dfc112469
Sunidhi <120239019+SunidhiSehwag@users.noreply.github.com>
2023-03-27 11:56:27 +0530

    This is the code for the bot

---
+++ code.py
@@ -0,0 +1,8 @@
+user_input = input("enter a command\n")
+
+if user_input.lower() == "ping":
+    print("pong")
+    user_input1=input("enter command\n")
+    if user_input1.lower() == "hi":
+       print("greetings")
+    
